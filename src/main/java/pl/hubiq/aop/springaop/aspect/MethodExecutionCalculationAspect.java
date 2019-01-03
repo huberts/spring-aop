ff331190e82ea6ed004d23f29f193834b88de6b3
Hubert Świetlicki <hswietlicki@systherm-info.pl>
2019-01-03 11:01:43 +0100

    Add @Around aspect

---
+++ src/main/java/pl/hubiq/aop/springaop/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,25 @@
+package pl.hubiq.aop.springaop.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Around("execution(* pl.hubiq.aop.springaop.business.*.*(..))")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        logger.info("Time taken by {} is {}", proceedingJoinPoint, timeTaken);
+        return result;
+    }
+}
